13daf8f6db557491cfcc0ed4f834c19c0bed902f
Unique Mlungu <uniquemlungu04@gmail.com>
2026-09-20 15:21:45 +0300

    Add founder and support information

--- app/main/routes.py
+++ app/main/routes.py
@@ -15,6 +15,11 @@ def home():
     return render_template("main/home.html")
 
 
+@main.route("/about")
+def about():
+    return render_template("main/about.html")
+
+
 @main.route("/scan", methods=["POST", "GET"])
 def scan():
 
